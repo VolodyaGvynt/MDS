511398adbcdf097e656757f4515ec0597701c75e
Gvynt <vova.bartosevych@gmail.com>
2025-07-13 22:54:34 +0200

    unreal commit

---
+++ MDS/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MDS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
